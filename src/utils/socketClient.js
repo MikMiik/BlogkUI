b9691a2b13fc157b156ee2d569fb4118fb30fff9
MikMik <minh0936532430@gmail.com>
2025-08-10 16:55:21 +0700

    update

--- src/utils/socketClient.js
+++ src/utils/socketClient.js
@@ -1,13 +1,13 @@
 import PusherJS from "pusher-js";
 
 const socketClient = new PusherJS("app1", {
-  wsHost: "api.blogk.online",
-  wsPort: 6001,
-  cluster: "ap1",
-  forceTLS: false,
-  encrypted: true,
-  disableStats: true,
-  enabledTransports: ["ws", "wss"],
+  wsHost: import.meta.env.VITE_WSHOST,
+  wsPort: import.meta.env.VITE_WSPORT,
+  cluster: import.meta.env.VITE_CLUSTER,
+  forceTLS: import.meta.env.VITE_FORCE_TLS,
+  encrypted: import.meta.env.VITE_ENCRYPTED,
+  disableStats: import.meta.env.VITE_DISABLE_STATS,
+  enabledTransports: import.meta.env.VITE_ENABLED_TRANSPORTS.split(","),
 });
 
 export default socketClient;
