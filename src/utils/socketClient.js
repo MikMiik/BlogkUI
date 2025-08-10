fe674ecce3f913167e90723d67bee7c3b77bc512
MikMik <minh0936532430@gmail.com>
2025-08-10 16:57:59 +0700

    update

--- src/utils/socketClient.js
+++ src/utils/socketClient.js
@@ -3,6 +3,7 @@ import PusherJS from "pusher-js";
 const socketClient = new PusherJS("app1", {
   wsHost: import.meta.env.VITE_WSHOST,
   wsPort: import.meta.env.VITE_WSPORT,
+  wssPort: import.meta.env.VITE_WSSPORT,
   cluster: import.meta.env.VITE_CLUSTER,
   forceTLS: import.meta.env.VITE_FORCE_TLS,
   encrypted: import.meta.env.VITE_ENCRYPTED,
